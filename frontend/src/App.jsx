f3e8248e02b5bb1a127641dc17ef3ebf550a4205
JayLukas <Jesselfsmit@gmail.com>
2026-05-04 22:32:19 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import { Toaster } from "@/components/ui/sonner";
